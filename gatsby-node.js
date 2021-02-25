6d78fe55791fe708720b07dc7ce0475f67122023
Bilal Elreda <bilal.codes@pm.me>
2021-02-24 20:48:38 -0800

    handle 'no jobs' scenario

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,37 @@
 const fetch = require('node-fetch');
-
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  createTypes(`
+    type JobDescription implements Node {
+      id: Int
+      name: String
+      body: String
+      record_type: String
+      record_id: Int
+      created_at: Date
+      updated_at: Date
+    }
+    type Job implements Node {
+      _id: String
+      title: String
+      slug: String
+      department: String
+      department_id: Int
+      description: JobDescription
+      screener_question_1: String
+      screener_question_2: String
+      screener_question_3: String
+      created_at: Date
+    }
+    type Career implements Node {
+      _id: String
+      title: String
+      slug: String
+      department: String
+      created_at: Date
+    }
+  `)
+}
 exports.sourceNodes = async ({
   actions,
   createNodeId,
@@ -8,21 +40,29 @@ exports.sourceNodes = async ({
   const {createNode} = actions;
   const {url, careersEndpoint} = configOptions;
   const baseUrl = `${url}/${careersEndpoint}`;
-
   const careers = await fetch(baseUrl)
     .then((response) => response.json())
     .catch((error) => console.log(error));
-
+  careers.forEach((career) => {
+    createNode({
+      ...career,
+      id: createNodeId(`position-${career.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Career',
+        content: JSON.stringify(career),
+        contentDigest: createContentDigest(career),
+      },
+    });
+  });
   const details = [];
-
   for (const career of careers) {
     const careerDetail = await fetch(`${baseUrl}/${career.slug}`)
       .then(async(response) => response.json())
       .catch((error) => error);
-    
       details.push(careerDetail.position)
   }
-
   details.forEach((detail) => 
     createNode({
       ...detail,
@@ -30,18 +70,11 @@ exports.sourceNodes = async ({
       parent: null,
       children: [],
       internal: {
-        type: 'jobs',
+        type: 'Job',
         content: JSON.stringify(detail),
         contentDigest: createContentDigest(detail),
       },
     })
   );
 }
-
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+exports.onPreInit = () => console.log("Loaded gatsby-source-deutsch-hr-api")
